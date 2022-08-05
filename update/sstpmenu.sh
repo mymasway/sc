49d0da497773c496d2d190599dd8289817751b9e
MasWay <70774862+mymasway@users.noreply.github.com>
2022-08-05 15:21:03 +0700

    Add files via upload

---
+++ update/sstpmenu.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+clear
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+yl='\e[32;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+bd='\e[1m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+# COLOR VALIDATION
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+#warna
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m          ⇱ Menu SSTP MasWayVPN ⇲          \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "❇️ 1) Create Account SSTP                                          "
+echo -e "❇️ 2) Delete Account SSTP                                             "
+echo -e "❇️ 3) Extending Account SSTP Active Life                                                "
+echo -e "❇️ 4) Check User Login SSTP                                                "       
+echo -e "❇️ 5) Menu Utama                                            "
+echo -e "❇️ 6) EXIT                                                        "
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m            ⇱ Script By MasWayVPN ⇲             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+read -p "Select From Options [ 1 - 6 ] : " menu
+echo -e ""
+case $menu in
+1)
+addsstp
+;;
+2)
+delsstp
+;;
+3)
+renewsstp
+;;
+4)
+ceksstp
+;;
+5)
+clear
+menu
+;;
+6)
+clear
+exit
+;;
+*)
+clear
+menu
+;;
+esac
---
+++ update/trgomenu.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+clear
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+yl='\e[32;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+bd='\e[1m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+# COLOR VALIDATION
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+#warna
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m          ⇱ Menu TROJAN GO MasWayVPN ⇲          \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "❇️ 1) Create Account Trojan Go                                          "
+echo -e "❇️ 2) Delete Account Trojan Go                                             "
+echo -e "❇️ 3) Extending Account Trojan Go Active Life                                                "
+echo -e "❇️ 4) Check User Login Trojan Go                                                "       
+echo -e "❇️ 5) Menu Utama                                            "
+echo -e "❇️ 6) EXIT                                                        "
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m            ⇱ Script By MasWayVPN ⇲             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+read -p "Select From Options [ 1 - 6 ] : " menu
+echo -e ""
+case $menu in
+1)
+addtrgo
+;;
+2)
+deltrgo
+;;
+3)
+renewtrgo
+;;
+4)
+cektrgo
+;;
+5)
+clear
+menu
+;;
+6)
+clear
+exit
+;;
+*)
+clear
+menu
+;;
+esac
